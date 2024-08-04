1ff71cd760448cc65d2aa33088597843629ec0c3
krrakshit <rkrkr365@gmail.com>
2024-08-04 23:52:05 +0530

    Create hmap02.java

---
+++ hmap02.java
@@ -0,0 +1,148 @@
+import java.util.*;
+
+public class hmap02 {
+    static class Hashmap<K, V> { // generics
+        private class Node {
+            K key;
+            V value;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+        private int n;
+        private int N;
+        private LinkedList<Node> buckets[];
+
+        @SuppressWarnings("unchecked")
+        public Hashmap() {
+            this.N = 4;
+            this.buckets = new LinkedList[4];
+            for (int i = 0; i < 4; i++) {
+                this.buckets[i] = new LinkedList<>();
+            }
+        }
+
+        private int hashfunction(K key) {
+            int hc = key.hashCode();
+
+            return Math.abs(hc) % N;
+        }
+
+        private int searchinll(K key, int bi) {
+            LinkedList<Node> ll = buckets[bi];
+            int di = 0;
+            for (int i = 0; i < ll.size(); i++) {
+                Node node = ll.get(i);
+                if (node.key == key) {
+                    return di;
+                }
+                di++;
+            }
+
+            return -1;
+        }
+
+        @SuppressWarnings("unchecked")
+        private void rehash() {
+            LinkedList<Node> oldbucket[] = buckets;
+            buckets = new LinkedList[N * 2];
+            N = 2 * N;
+            for (int i = 0; i < buckets.length; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            for (int i = 0; i < oldbucket.length; i++) {
+                LinkedList<Node> ll = oldbucket[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.remove();
+                    put(node.key, node.value);
+                }
+            }
+        }
+
+        public void put(K key, V value) {
+
+            int bi = hashfunction(key);
+            int di = searchinll(key, bi);
+            if (di != -1) {
+                Node node = buckets[bi].get(di);
+                node.value = value;
+            } else {
+                buckets[bi].add(new Node(key, value));
+                n++;
+            }
+            double lambda = (double) n / N;
+            if (lambda > 2.0) {
+                rehash();
+            }
+        }
+
+        public boolean containskey(K key) {
+
+            int bi = hashfunction(key);
+            int di = searchinll(key, bi);
+            if (di != -1) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+
+        public V remove(K key) {
+
+            int bi = hashfunction(key);
+            int di = searchinll(key, bi);
+            if (di != -1) {
+                Node node = buckets[bi].remove(di);
+                n--;
+                return node.value;
+            } else {
+                return null;
+            }
+        }
+
+
+        public V get(K key) {
+
+            int bi = hashfunction(key);
+            int di = searchinll(key, bi);
+            if (di != -1) {
+                Node node = buckets[bi].get(di);
+                return node.value;
+            } else {
+                return null;
+            }
+        }
+
+        public ArrayList<K> keySet() {
+
+            ArrayList<K> keys = new ArrayList<>();
+
+            for (int i = 0; i < buckets.length; i++) {
+                LinkedList<Node> ll = buckets[i];
+                for (Node node : ll) {
+                    keys.add(node.key);
+                }
+            }
+            return keys;
+
+        }
+
+
+        public boolean isempty() {
+            return n == 0;
+        }
+    }
+
+    public static void main(String[] args) {
+            Hashmap<String,Integer> hm = new Hashmap<>();
+
+        hm.put("India",100);
+        hm.put("china",50);
+        hm.put("us",44);
+    }
+
+}
