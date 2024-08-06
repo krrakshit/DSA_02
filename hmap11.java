9bdc829cc032a6fe5829d1fa051fb1330a366614
krrakshit <rkrkr365@gmail.com>
2024-08-06 23:34:03 +0530

    Create hmap11.java

---
+++ hmap11.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class hmap11 {//o(n)
+    public static void main(String[] args) {
+        int arr[] = {15,-2,2,-8,1,7,10,23};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        // sum,idx pair
+        int sum = 0;
+        int len = 0;
+        for (int j = 0;j<arr.length;j++){
+            sum+= arr[j];
+            if (map.containsKey(sum)){
+                len = Math.max(len,j-map.get(sum));
+            }
+            else{
+                map.put(sum,j);
+            }
+        }
+        System.out.println("length of largest subarray is "+len);
+    }
+}
